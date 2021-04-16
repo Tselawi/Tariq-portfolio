d5d6a4baaae407f281acef441721d7167a331b0f
Tariq <selawi.be@gmail.com>
2021-04-16 23:51:52 +0200

    init

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import Person from "../assets/PersonalData";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div id="contact" className="container-fluid padding" bg="dark">
+      <div className="row headers text-center padding">
+        <div className="col-12">
+          <h1 className="display-4">Contact Me</h1>
+        </div>
+        <hr />
+      </div>
+      <div className="row padding">
+        <div className="col-sm-12 col-md-6 col-lg-6">
+          <div className="container-fluid padding">
+            <div className="row text-center padding">
+              <div className="col-12 p-5">
+                <h2>Follow Me</h2>
+                <ul className="col-12 media d-flex justify-content-center list-inline">
+                  {Person.socialMedias.map((media, index) => (
+                    <li key={index} className="list-inline-item">
+                      <a
+                        href={media.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className={media.icon} />
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" text-center p-5 col-sm-12 col-md-6 col-lg-6">
+          <h2 className="pb-2">I'm Ready Let's Talk</h2>
+          <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                className="form-control"
+                name="message"
+                placeholder="Your Message"
+                rows="6"
+                required
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn-outline-primary btn-lg">
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Person from "../assets/PersonalData";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="container-fluid padding">
+        <div className="row text-center">
+          <div className="col-md-4">
+            <img
+              src={Person.about.logo}
+              className="mx-3"
+              width="50"
+              height="52"
+              alt="logo"
+            />
+            <span>Tariq Selawi</span>
+            <hr className="light" />
+            <p>+32466-34-6060</p>
+            <p>selawi.be@gmail.com</p>
+          </div>
+          <div className="col-md-4">
+            <hr className="light" />
+            <h5>Address</h5>
+            <hr className="light" />
+            <p>Avenue de l'observatoire 26</p>
+            <p>Liège, Belgium</p>
+          </div>
+          <div className="col-md-4">
+            <hr className="light" />
+            <h5>Social Media</h5>
+            <hr className="light" />
+            {Person.socialMedias.map((media, index) => (
+              <p key={index} className="footer-media">
+                <a href={media.url} target="_blank" rel="noopener noreferrer">
+                  {media.name}
+                </a>
+              </p>
+            ))}
+          </div>
+          <div className="col-12">
+            <hr className="light-100" />
+            <h5 className="footer-media">
+              &copy;2021{" "}
+              <a href="" target="_blank" rel="noopener noreferrer">
+                Tariq Selawi
+              </a>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
